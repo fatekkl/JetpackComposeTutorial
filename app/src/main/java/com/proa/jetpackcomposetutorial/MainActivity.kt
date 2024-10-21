772ea003daa5091ed1f494930234dbf83467465c
fatekkl <mathtml.1105@gmail.com>
2024-10-21 11:10:52 -0300

    testing composables

--- app/src/main/java/com/proa/jetpackcomposetutorial/MainActivity.kt
+++ app/src/main/java/com/proa/jetpackcomposetutorial/MainActivity.kt
@@ -4,12 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.proa.jetpackcomposetutorial.ui.theme.JetpackComposeTutorialTheme
 
@@ -20,9 +23,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             JetpackComposeTutorialTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                    MessageCard(
+                        Message("Matheus", "Hello!!"),
+                        Modifier.padding(innerPadding)
                     )
                 }
             }
@@ -30,12 +33,38 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+data class Message(val author: String, val body: String)
+
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
         modifier = modifier
     )
+}
+
+
+@Composable
+fun MessageCard(msg: Message, modifier: Modifier){
+
+
+    Column {
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher_background),
+            contentDescription = "Android"
+        )
+
+        Text(
+            text =  msg.author,
+            modifier = modifier
+        )
+
+        Text(
+            text = msg.body,
+            modifier = modifier
+        )
+    }
+
 }
 
 @Preview(showBackground = true)
